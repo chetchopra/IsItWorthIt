ee4f1b9b2d294ca6e3831320a90f916fc6e0583b
jenshaw86 <bloo.j.shaw@gmail.com>
2019-07-09 16:42:10 -0700

    Fetch and display wishlist items

--- scripts/main.js
+++ scripts/main.js
@@ -4,8 +4,19 @@ const url = "http://localhost:3000/items"
 function fetchWishlistItems() {
     fetch(url)
     .then(resp => resp.json())
-    .then(json => console.log(json))
+    .then(json => displayWishlist(json))
     .catch(err => err.message)
+}
+
+function displayWishlist(items) {
+    while (wishlist.firstChild) {
+        wishlist.removeChild(wishlist.firstChild);
+    }
+    items.forEach(item => {
+        let li = document.createElement("li");
+        li.textContent = item.name;
+        wishlist.appendChild(li);
+    })
 }
 
 fetchWishlistItems();

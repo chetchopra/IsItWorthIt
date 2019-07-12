2f6a3f8d9611cc059bad0d5f331e2ff60dfbf449
chetchopra <chetanya.chopra94@gmail.com>
2019-07-11 23:35:25 -0700

    Main.js files split into 3 files. Main.js, adEventListeners.js, and globals.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,57 +1,3 @@
-// API endpoints
-const wishlistUrl = "http://localhost:3000/items"
-const comparisonUrl = "http://localhost:3000/comparison_items"
-const userUrl = "http://localhost:3000/users";
-
-// Nav bar elements
-const navLoginBtn = document.querySelector("#nav-login-btn");
-const navWishListBtn = document.querySelector("#wish-list-btn");
-const navLogoutBtn = document.querySelector("#logout-btn");
-const loginBtn = document.querySelector("#login-btn");
-
-// Login/Signup elemets 
-const signUpBtn = document.querySelector("#sign-up-btn");
-const newUserName = document.querySelector("#new-username");
-const userName = document.querySelector("#username");
-const loginModal = document.querySelector("#loginModal");
-
-// Wishlist
-const wishlist = document.querySelector(".wishlist-body ul");
-const slideMenu = document.querySelector("#side-menu");
-const exitMenuBtn = document.querySelector("#exit-list-btn");
-
-// Left panel - Item name/cost
-const leftCompare = document.querySelector(".compare-left form");
-const name = leftCompare.children[0].children[0];
-const cost = leftCompare.children[1].children[0];
-
-
-// const rightCompare = document.querySelector(".compare-right form"); //not used
-
-// Right panel - Comparison items dropdown
-const dropdown = document.querySelector("select");
-
-// Bottom panel - numeric/visual results, buttons
-const resultHeader = document.querySelector("#result-header");
-const resultCompare = document.querySelector("#result-compare");
-const resultsDiv = document.querySelector(".results");
-const resultBtns = document.querySelector("#result-btns");
-const worthItBtn = document.querySelector("#worth-it");
-const notWorthItBtn = document.querySelector("#not-worth-it");
-const resultVisual = document.querySelector("#result-visual");
-
-// Read modal elements 
-const seeItemModal = document.querySelector("#seeItemModal");
-const wishItemNameCell = document.querySelector("#show-item-name");
-const wishItemCostCell = document.querySelector("#show-item-cost");
-const editBtn = document.querySelector("#edit-btn");
-const deleteBtn = document.querySelector("#delete-btn")
-
-// Edit modal elements
-const editItemField = document.querySelector("#edit-item");
-const editCostField = document.querySelector("#edit-item-cost");
-const editConfirmBtn = document.querySelector("#edit-confirm-btn");
-
 /* All fetches */
 
 // Creates a new user in the database
@@ -81,7 +27,7 @@ function fetchWishlistItems() {
     .then(json => {
         displayWishlist(json.items);
     })
-    .catch(err => err.message)
+    .catch(err => err.message);
 }
 
 // Fetches all the comparison items
@@ -89,14 +35,14 @@ function fetchComparisonItems() {
     fetch(comparisonUrl)
     .then(resp => resp.json())
     .then(json => displayComparisonItems(json))
-    .catch(err => err.message)
+    .catch(err => err.message);
 }
 
 // Fetches a single comparison item
 function fetchComparisonItem(cost, id) {
     fetch(`${comparisonUrl}/${id}`)
     .then(resp => resp.json())
-    .then(obj => displayItemCount(cost, obj))
+    .then(obj => displayItemCount(cost, obj));
 }
 
 // Adds a new wishlist item to the database
@@ -117,7 +63,7 @@ function addWishItem() {
     })
     .then(resp => resp.json())
     .then(obj => fetchWishlistItems())
-    .catch(err => err.message)
+    .catch(err => err.message);
 }
 
 // Updates the database with the selected items with information 
@@ -153,7 +99,7 @@ function deleteItem() {
     })
     .then(resp => resp.json())
     .then(json => fetchWishlistItems())
-    .catch(err => err.message)
+    .catch(err => err.message);
 }
 
 /* All fetches */
@@ -206,6 +152,10 @@ function closeList() {
 function seeItem(item) {
     wishItemNameCell.textContent = item.name;
     wishItemCostCell.textContent = `$${item.cost.toFixed(2)}`;
+    setCurrentItemInLocalStorage(item);
+}
+
+function setCurrentItemInLocalStorage(item) {
     localStorage.removeItem("cur_item_id");
     localStorage.removeItem("cur_item_name");
     localStorage.removeItem("cur_item_cost");
@@ -218,7 +168,6 @@ function seeItem(item) {
 function setEditModalValues() {
     let itemName = localStorage.getItem("cur_item_name");
     let itemCost = localStorage.getItem("cur_item_cost");
-    console.log(`Setting modal values ${itemName}  ${itemCost}`);
     editItemField.setAttribute("value", itemName);
     editCostField.setAttribute("value", itemCost);
 }
@@ -311,7 +260,7 @@ function validateUser(response) {
         fetchWishlistItems();
         toggleBtns(response.id);
     } else {
-        userName.value = "Invalid Login"
+        userName.value = "Invalid Login";
     }
 }
 
@@ -324,7 +273,6 @@ function saveUserToLocalStorage(user) {
 // Checks local storage to see if a user is logged in
 function checkLocalStorage() {
     let userId = localStorage.getItem("user_id");
-    console.log(`User id: ${userId}`);
     if (userId) {
         toggleBtns(userId);
         fetchWishlistItems();
@@ -346,97 +294,6 @@ function toggleBtns(userId) {
 }
 
 /* User access */
-
-
-/* Event Listeners */
-
-// Adds event listener to edit button
-function addEditBtnEventListener() {
-    editBtn.addEventListener("click", setEditModalValues);
-}
-
-// Adds event listener to delete button
-function addDeleteBtnEventListener() {
-    deleteBtn.addEventListener("click", deleteItem);
-}
-
-// Adds event listener to confirm edit button
-function addEditConfirmBtnEventListener() {
-    editConfirmBtn.addEventListener("click", function () {
-        updateItem();
-    });
-}
-
-// Adds event listener to dropdown change
-function addDropdownEventListener() {
-    dropdown.addEventListener("change", populateItems);
-}
-
-// Adds event listener to worthit button
-function addWorthItBtnEventListener() {
-    worthItBtn.addEventListener("click", () => {
-        addWishItem();
-        clearResults();
-    })
-}
-
-// Adds event listener to notworthit button
-function addNotWorthBtnEventListener() {
-    notWorthItBtn.addEventListener("click", clearResults);
-}
-
-// Adds event listener to cost field on enter
-function addCostFieldEventListener() {
-    cost.addEventListener("keypress", (event) => {
-        var key = event.which || event.keyCode;
-        if (key == 13) {
-            populateItems();
-        }
-    });
-}
-
-// Adds event listener to signup link
-function addSignUpEventListener() {
-    signUpBtn.addEventListener("click", createNewUser)
-}
-
-// Adds event listener to login button
-function addLoginEventListener() {
-    loginBtn.addEventListener("click", loginUser);
-}
-
-// Adds event listener to close wishlist button
-function addSlideClosedEventListener() {
-    exitMenuBtn.addEventListener("click", closeList)
-}
-
-// Adds event listener to logout button
-function addLogoutEventListener() {
-    navLogoutBtn.addEventListener("click", logoutUser)
-}
-
-// Adds event listener to wishlist open
-function addSlideOpenEventListener() {
-    navWishListBtn.addEventListener("click", openList)
-}
-
-// Loads all event listeners
-function loadListeners() {
-    addDropdownEventListener();
-    addSignUpEventListener();
-    addLoginEventListener();
-    addLogoutEventListener();
-    addSlideOpenEventListener();
-    addSlideClosedEventListener();
-    addWorthItBtnEventListener()
-    addNotWorthBtnEventListener();
-    addCostFieldEventListener();
-    addEditBtnEventListener();
-    addDeleteBtnEventListener();
-    addEditConfirmBtnEventListener();
-}
-
-/* Event Listeners */
 
 
 /* main */
